623516f708034dd06cddaf54b8fdb5312dd96abf
Rehalya shree <94946567+rehalya2004@users.noreply.github.com>
2022-06-24 13:35:22 +0530

    Code to capture first frame of a  webcam-video

---
+++ Video Capturing_01.py
@@ -0,0 +1,19 @@
+import cv2, time
+
+video = cv2.VideoCapture(0)
+
+check, frame = video.read()
+
+print(check)
+print(frame)
+
+time.sleep(3)
+
+cv2.imshow("Capturing",frame)
+
+cv2.waitKey(0)
+
+video.release()
+
+
+
